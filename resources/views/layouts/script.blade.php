3fd766df8471d6b914c62e785dda4a50a8bf6daf
mohammadsaleh1 <mo12hammad3@hotmail.com>
2022-04-11 15:28:34 +0300

    add some changes

---
+++ resources/views/layouts/script.blade.php
@@ -0,0 +1,9 @@
+
+<!-- jQuery -->
+<script src="{{asset('assets/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('assets/plugins/bootstrap/js/bootstrap.bundle.js.map')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('assets/js/adminlte.min.js')}}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{asset('assets/js/demo.js')}}"></script>
